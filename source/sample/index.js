38b8b76ac36b66113cc9e1975208b48ebf326ef8
fedeghe <fedeghe@gmail.com>
2021-02-07 00:27:04 +0100

    demo fix

--- source/sample/index.js
+++ source/sample/index.js
@@ -38,10 +38,11 @@ Y(function () {
         var b = Y('<button>click</button>');
         Y('#four').append(b);
         b.on('click', function () {
-            var position = this.style(['left', 'right'])
-            this.move(position[0].right === '10px' ? 'left' : 'right');
+            var self = Y(this),
+                position = self.style(['left', 'right'])
+            self.move(position[0].right === '10px' ? 'left' : 'right');
         })
-        Y.extend(function move(where, other) {
+        Y.extend(function move(where) {
             where === 'right' 
             ? this.style({position:'absolute', [where]: '10px', left:'', top: '10px'})
             : this.style({position:'absolute', [where]: '10px', right: '', top: '10px'});
